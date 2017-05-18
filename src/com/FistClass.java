0443500da564e5d29d58ef7414529ddc01b06e85
pjm2010 <prithvijit20@gmail.com>
2017-05-19 01:05:16 +0530

    Add files via upload

---
+++ src/com/FistClass.java
@@ -0,0 +1,12 @@
+package com;
+
+public class FistClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//Display of "Hello World"
+		System.out.println("Hello World");
+	}
+
+}
